84935caefdea8b5b157bdf85641934318133eb19
Braian <villasantibraian@icloud.com>
2022-02-18 17:11:47 -0300

    initial commit

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+import heroImage from "../images/hero-image.png"
+
+export default function Hero() {
+    return (
+        <section className="hero">
+            <img src={heroImage} alt="Hero"></img>
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+    )
+}

5fabd43d89d4eb78f77d5b0899d5a6f7865d7b93
Braian <villasantibraian@icloud.com>
2022-02-24 11:13:13 -0300

    sold out badge

--- src/App.js
+++ src/App.js
@@ -3,13 +3,40 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import './styles.css'
 import Card from './components/Card';
+import data from './data'
+
+/*
+Challenge:
+
+- import the array of data from data.js
+- map over the array to create <Card /> components
+- display the array of card components under the navbar
+  (in place of the current <Card /> component)
+
+Note: We haven't styled the group of components yet, so they'll
+still be block elements, stacked vertically. We'll add styling later.
+*/
 
 export default function App() {
+    let cards = data.map(d => <Card
+            key={d.id}
+            img={d.coverImg}
+            rating={d.stats.rating}
+            reviewCount={d.stats.reviewCount}
+            location={d.location}
+            title={d.title}
+            price={136}
+            openSpots={d.openSpots}
+        />
+    );
+    console.log(cards);
     return (
         <div>
             <Navbar />
             <Hero />
-            <Card />
+            <section className="cards-list">
+                {cards}
+            </section>
         </div>
     );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,25 @@
 import React from "react";
-import cardImage from "../images/katie-zaferes.png"
 import starIcon from "../images/star.png"
 
-export default function Card() {
+export default function Card(props) {
+    let badgeText; 
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT";
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE";
+    }
     return (
         <div className="card">
-            <img className="card-image" src={cardImage}/>
+            {badgeText && <div className="card-badge">{badgeText}</div>}
+            <img className="card-image" alt="" src={'./images/' + props.img}/>
             <div className="card-info">
-                <img className="star-icon" src={starIcon} width="14" height="14"/>
-                <span className="star-rating">5.0</span>
-                <span className="location">(6) • USA</span>
+                <img className="star-icon" src={starIcon} alt="star" width="14" height="14"/>
+                <span className="star-rating">{props.rating}</span>
+                <span className="location">({props.reviewCount}) • {props.location}</span>
             </div>
-            <p className="card-desc">Life lessons with Katie Zaferes</p>
+            <p className="card-desc">{props.title}</p>
             <p>
-                <span className="price">From $136</span> / person
+                <span className="price">From ${props.price}</span>
             </p>
         </div>
     )
